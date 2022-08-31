390c058457a2c6e6e0fb7e472975216e3202b7a9
lucas51512 <lucasbrancalhao454@gmail.com>
2022-08-31 20:44:10 -0300

    Arquivos com funções de inclusão, menos o emprétimo

--- Alunos.cpp
+++ Alunos.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 #include <math.h>
+#include <string.h>
+
+struct indice {
+    int numeroIndice;
+    int enderecoFisico;
+};
 
 struct aluno {
     int ra;
@@ -8,11 +14,6 @@ struct aluno {
     char telefone[14];
     char cidadeAluno[35];
     char uf[2];
-};
-
-struct indiceAlunos {
-    int numeroIndiceAluno;
-    int enderecoFisicoAluno;
 };
 
 struct editora {
@@ -24,21 +25,11 @@ struct editora {
     char uf[2];
 };
 
-struct indiceEditora {
-    int numeroIndiceEditora;
-    int enderecoFisicoEditora;
-};
-
 struct livro {
    int codigoLivro;
    char descricaoLivro[50];
    int codigoEditora;
    char situacaoLivro[1];
-};
-
-struct indiceLivro {
-    int numeroIndiceAluno;
-    int enderecoFisicoAluno;
 };
 
 struct emprestimo {
@@ -50,33 +41,40 @@ struct emprestimo {
     char situacaoEmprestimo[1];
 };
 
-struct indiceEmprestimo {
-    int numeroIndiceEmprestimo;
-    int enderecoFisicoEmprestimo;
-};
-
 
 using namespace std;
 
 int main(){
+    int quantidadeAlunos;
+    int quantidadeEditora;
+    int quantidadeLivros;
     int termina = 0;
 
     switch (termina != 10){
+        cout << "1 - Incluir registros" << endl;
     case 1:
         
         break;
-    
-    default:
-        break;
+ 
     }
 
 }
 
-aluno leiaAluno(){
-    int quantidadeAlunos;
+indice leiaTabelaIndiceAlunos(int quantidadeAlunos){
+    indice novaTabelaIndiceAlunos[quantidadeAlunos];
 
-    cout << "Informe a quantidade de alunos para serem adicionados: ";
-    cin >> quantidadeAlunos;
+    for(int i = 0; i < quantidadeAlunos; i++){
+        cout << "Informe o indice do aluno numero: " << i << ":";
+        cin >> novaTabelaIndiceAlunos[i].numeroIndice;
+
+        cout << "Informe o endereço físico do aluno numero:" << i << "";
+        cin >> novaTabelaIndiceAlunos[i].enderecoFisico;
+    }
+
+    return novaTabelaIndiceAlunos[quantidadeAlunos];
+}
+
+aluno leiaAluno(int quantidadeAlunos){
 
     aluno novaTabelaAlunos[quantidadeAlunos];
 
@@ -103,11 +101,22 @@ aluno leiaAluno(){
     return novaTabelaAlunos[quantidadeAlunos];
 }
 
-editora leiaEditora(){
-    int quantidadeEditora;
+indice leiaTabelaIndiceEditora(int quantidadeEditora){
+    indice novaTabelaIndiceEditora[quantidadeEditora];
 
-    cout << "Informe a quantidade de editoras para serem adicionadas: ";
-    cin >> quantidadeEditora;
+    for(int i = 0; i < quantidadeEditora; i++){
+        cout << "Informe o indice da editora numero: " << i << ":";
+        cin >> novaTabelaIndiceEditora[i].numeroIndice;
+
+        cout << "Informe o endereço físico da editora numero:" << i << "";
+        cin >> novaTabelaIndiceEditora[i].enderecoFisico;
+    }
+
+    return novaTabelaIndiceEditora[quantidadeEditora];
+}
+
+editora leiaEditora(int quantidadeEditora){
+
 
     editora novaTabelaEditoras[quantidadeEditora];
 
@@ -134,6 +143,20 @@ editora leiaEditora(){
     return novaTabelaEditoras[quantidadeEditora];
 }
 
+indice leiaTabelaIndiceLivros(int quantidadeLivros){
+    indice novaTabelaIndiceLivros[quantidadeLivros];
+
+    for(int i = 0; i < quantidadeLivros; i++){
+        cout << "Informe o indice do livro numero: " << i << ":";
+        cin >> novaTabelaIndiceLivros[i].numeroIndice;
+
+        cout << "Informe o endereço físico do aluno numero:" << i << "";
+        cin >> novaTabelaIndiceLivros[i].enderecoFisico;
+    }
+
+    return novaTabelaIndiceLivros[quantidadeLivros];
+}
+
 livro leiaLivros(){
     int quantidadeLivro;
 
@@ -158,4 +181,218 @@ livro leiaLivros(){
     }
 
     return novaTabelaLivros[quantidadeLivro];
+}
+
+void inclusaoAluno (indice index[], aluno listaAlunos[], int &contador, int codigo){
+    contador++;
+
+    listaAlunos[contador].ra = codigo;
+
+    cout << "Nome Aluno: ";
+    cin >> listaAlunos[contador].nome;
+
+    cout << "Endereco Aluno: ";
+    cin >> listaAlunos[contador].enderecoAluno;
+
+    cout << "Telefone Aluno: ";
+    cin >> listaAlunos[contador].telefone;
+
+    cout << "Cidade Aluno: ";
+    cin >> listaAlunos[contador].cidadeAluno;
+
+    cout << "Estado Aluno: ";
+    cin >> listaAlunos[contador].uf;
+
+    //Inclusão na área de indices
+    int i;
+    for(i = contador - 1; index[i].numeroIndice > codigo; i--){
+        index[i+1].numeroIndice = index[i].numeroIndice;
+        index[i+1].enderecoFisico = index[i].enderecoFisico;
+    }
+    index[i+1].numeroIndice = codigo;
+    index[i+1].enderecoFisico = contador;
+    cout << "Inclusão realizada com sucesso";
+}
+
+void buscaAlunos(indice index[], aluno listaAlunos[], int &contador, int codigo) {
+    int i = 0, f = contador;
+    int m = (i + f) /2;
+    for(; f >= i && codigo != index[m].numeroIndice; m = (i + f) /2){
+        if(codigo > index[m].numeroIndice)
+            i = m + 1;
+        else
+            f = m-1;
+    }
+    if(codigo == index[m].numeroIndice){
+        cout << "\n\n Aluno ja Cadastrado - não pode ser cadastrado novamente";
+        i = index[m].enderecoFisico;
+        cout << "\nCódigo do Aluno: " << listaAlunos[i].ra;
+        cout << "\tNome: " << listaAlunos[i].nome;
+        cout << "\tEndereco: " << listaAlunos[i].enderecoAluno;
+        cout << "\tTelefone: " << listaAlunos[i].telefone;
+        cout << "\tCidade: " << listaAlunos[i].cidadeAluno;
+        cout << "\tEstado: " << listaAlunos[i].uf;
+    }
+    else
+        inclusaoAluno(index, listaAlunos, contador, codigo);
+    
+}
+
+// Editora
+
+void inclusaoEditora (indice index[], editora listaEditora[], int &contador, int codigo){
+    contador++;
+
+    listaEditora[contador].codigo = codigo;
+
+    cout << "Nome Editora: ";
+    cin >> listaEditora[contador].nome;
+
+    cout << "Endereco Editora: ";
+    cin >> listaEditora[contador].enderecoEditora;
+
+    cout << "Telefone Editora: ";
+    cin >> listaEditora[contador].telefone;
+
+    cout << "Cidade Editora: ";
+    cin >> listaEditora[contador].cidadeEditora;
+
+    cout << "Estado Aluno: ";
+    cin >> listaEditora[contador].uf;
+
+    //Inclusão na área de indices
+    int i;
+    for(i = contador - 1; index[i].numeroIndice > codigo; i--){
+        index[i+1].numeroIndice = index[i].numeroIndice;
+        index[i+1].enderecoFisico = index[i].enderecoFisico;
+    }
+    index[i+1].numeroIndice = codigo;
+    index[i+1].enderecoFisico = contador;
+    cout << "Inclusão realizada com sucesso";
+}
+
+void buscaEditora(indice index[], editora listaEditora[], int &contador, int codigo) {
+    int i = 0, f = contador;
+    int m = (i + f) /2;
+    for(; f >= i && codigo != index[m].numeroIndice; m = (i + f) /2){
+        if(codigo > index[m].numeroIndice)
+            i = m + 1;
+        else
+            f = m-1;
+    }
+    if(codigo == index[m].numeroIndice){
+        cout << "\n\n Editora ja Cadastrada - não pode ser cadastrada novamente";
+        i = index[m].enderecoFisico;
+        cout << "\nCódigo da Editora: " << listaEditora[i].codigo;
+        cout << "\tNome: " << listaEditora[i].nome;
+        cout << "\tEndereco: " << listaEditora[i].enderecoEditora;
+        cout << "\tTelefone: " << listaEditora[i].telefone;
+        cout << "\tCidade: " << listaEditora[i].cidadeEditora;
+        cout << "\tEstado: " << listaEditora[i].uf;
+    }
+    else
+        inclusaoEditora(index, listaEditora, contador, codigo);
+    
+}
+
+void inclusaoLivro(indice index[], livro listaLivros[], int &contador, int codigo){
+    contador++;
+
+    listaLivros[contador].codigoLivro;
+
+    cout << "Descricao Livro: ";
+    cin >> listaLivros[contador].descricaoLivro;
+
+    cout << "Codigo Editora: ";
+    cin >> listaLivros[contador].codigoEditora;
+
+    cout << "Situação Livro: ";
+    cin >> listaLivros[contador].situacaoLivro;
+
+    //Inclusão na área de indices
+    int i;
+    for(i = contador - 1; index[i].numeroIndice > codigo; i--){
+        index[i+1].numeroIndice = index[i].numeroIndice;
+        index[i+1].enderecoFisico = index[i].enderecoFisico;
+    }
+    index[i+1].numeroIndice = codigo;
+    index[i+1].enderecoFisico = contador;
+    cout << "Inclusão realizada com sucesso";
+}
+
+void buscaLivros(indice index[], livro listaLivros[], int &contador, int codigo) {
+    int i = 0, f = contador;
+    int m = (i + f) /2;
+    for(; f >= i && codigo != index[m].numeroIndice; m = (i + f) /2){
+        if(codigo > index[m].numeroIndice)
+            i = m + 1;
+        else
+            f = m-1;
+    }
+    if(codigo == index[m].numeroIndice){
+        cout << "\n\n Livro ja Cadastrado - não pode ser cadastrado novamente";
+        i = index[m].enderecoFisico;
+        cout << "\nCódigo do Livro: " << listaLivros[i].codigoLivro;
+        cout << "\tDescricao do Livro: " << listaLivros[i].descricaoLivro;
+        cout << "\tCodigo da Editora: " << listaLivros[i].codigoEditora;
+        cout << "\tSituacao: " << listaLivros[i].situacaoLivro;
+    }
+    else
+        inclusaoLivro(index, listaLivros, contador, codigo);
+    
+}
+
+void inclusaoEmprestimo(indice index[], emprestimo listaEmprestimo[], int &contador, int codigo){
+    contador++;
+
+    listaAlunos[contador].ra = codigo;
+
+    cout << "Nome Aluno: ";
+    cin >> listaAlunos[contador].nome;
+
+    cout << "Endereco Aluno: ";
+    cin >> listaAlunos[contador].enderecoAluno;
+
+    cout << "Telefone Aluno: ";
+    cin >> listaAlunos[contador].telefone;
+
+    cout << "Cidade Aluno: ";
+    cin >> listaAlunos[contador].cidadeAluno;
+
+    cout << "Estado Aluno: ";
+    cin >> listaAlunos[contador].uf;
+
+    //Inclusão na área de indices
+    int i;
+    for(i = contador - 1; index[i].numeroIndice > codigo; i--){
+        index[i+1].numeroIndice = index[i].numeroIndice;
+        index[i+1].enderecoFisico = index[i].enderecoFisico;
+    }
+    index[i+1].numeroIndice = codigo;
+    index[i+1].enderecoFisico = contador;
+    cout << "Inclusão realizada com sucesso";
+}
+
+void buscaAlunos(indice index[], aluno listaAlunos[], int &contador, int codigo) {
+    int i = 0, f = contador;
+    int m = (i + f) /2;
+    for(; f >= i && codigo != index[m].numeroIndice; m = (i + f) /2){
+        if(codigo > index[m].numeroIndice)
+            i = m + 1;
+        else
+            f = m-1;
+    }
+    if(codigo == index[m].numeroIndice){
+        cout << "\n\n Aluno ja Cadastrado - não pode ser cadastrado novamente";
+        i = index[m].enderecoFisico;
+        cout << "\nCódigo do Aluno: " << listaAlunos[i].ra;
+        cout << "\tNome: " << listaAlunos[i].nome;
+        cout << "\tEndereco: " << listaAlunos[i].enderecoAluno;
+        cout << "\tTelefone: " << listaAlunos[i].telefone;
+        cout << "\tCidade: " << listaAlunos[i].cidadeAluno;
+        cout << "\tEstado: " << listaAlunos[i].uf;
+    }
+    else
+        inclusaoAluno(index, listaAlunos, contador, codigo);
+    
 }
